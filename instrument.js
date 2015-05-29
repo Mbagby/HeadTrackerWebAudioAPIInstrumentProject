41eb3db6ebfe41d70c48dd88118116e17e0f8d62
Madeline <madelinebagby@yahoo.com>
2015-05-29 15:47:32 -0700

    Instrument project

---
+++ instrument.js
@@ -0,0 +1,25 @@
+// var videoInput = document.getElementById('inputVideo');
+// var canvasInput = document.getElementById('inputCanvas');
+// var htracker = new headtrackr.Tracker();
+// htracker.init(videoInput, canvasInput);
+// htracker.start();
+
+// var context = new AudioContext ();
+// var oscillator = context.createOscillator();
+// oscillator.connect(context.destination);
+// oscillator.start();
+
+// gainNode = context.createGain();
+// gainNode.gain.value = 1.5;
+// oscillator.connect(gainNode);
+// gainNode.connect(context.destination);
+
+  // htracker.addEventListener("found", function(event){
+
+       // })
+
+// convolverNode = context.createConvolver();
+// convolverNode.convolver.value = 2;
+// oscillator.connect(convolverNode);
+// convolverNode.connect(context.destination);
+
